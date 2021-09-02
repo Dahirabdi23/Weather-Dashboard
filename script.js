965be4cac9d7218963dac254bd7aefd9f0a1f0a0
Dahir Abdi <dahirabdi@Dahirs-MacBook-Pro.local>
2021-09-01 22:55:47 -0400

    succesfull in get the data from the server and was able to put in the html

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-var APikey = "e5ce6a94e5d16a0e40da8ab1f7da8b25"
+var cityname = document.querySelector('#cityname')
+var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind')
+var humidity = document.querySelector('#humidity')
+var Uvindex = document.querySelector('#UVindex')
+var city;
+var button = document.querySelector("#search-button")
+function handlesearchinput(event) {
+    event.preventDefault();
+    var searchinputval =document.querySelector("#search-input");
+    
+    if (!searchinputval) {
+        alert("pls search a city")
+        return;
+    }
+}
+var searchinputval =document.querySelector("#search-input");
+button.addEventListener("click", function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+searchinputval.value+'&appid=e5ce6a94e5d16a0e40da8ab1f7da8b25')
+    .then(response => response.json())
+    .then(data => {
+        var nameval = data['name'];
+        var tempval = data['main']['temp'];
+        var humidityval = data['main']['humidity'];
+        var windval = data['wind']['speed'];
+        
+        cityname.innerHTML= nameval;
+        temp.innerHTML = tempval;
+        wind.innerHTML = windval;
+        humidity.innerHTML = humidityval;
+    })
+});
